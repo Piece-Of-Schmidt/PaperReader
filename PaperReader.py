9605f66cbeff69724212ffc51ce76361dd9bda4f
Tobias Schmidt <106234803+Piece-Of-Schmidt@users.noreply.github.com>
2024-02-11 10:26:58 +0100

    include newsletter functionality

--- PaperReader.py
+++ PaperReader.py
@@ -4,20 +4,39 @@ import re
 import os
 import glob
 import smtplib
+import random
+from fpdf import FPDF
 from openai import OpenAI
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 
+class PDF(FPDF):
+    def header(self):
+        self.set_font('helvetica', 'B', 12)
+        self.cell(0, 10, 'Portfolio', 0, 1, 'C')
+
+    def chapter_title(self, title):
+        self.set_font('helvetica', 'B', 12)
+        self.cell(0, 10, title, 0, 1, 'L')
+        self.ln(10)
+
+    def chapter_body(self, body):
+        self.set_font('helvetica', '', 12)
+        self.multi_cell(0, 10, body)
+        self.ln()
+
 class ResearchAssistant:
 
-    def __init__(self, settings_path='settings.csv'):
+    def __init__(self, settings_path='settings.csv', portfolio_maker=None):
         self.settings = self.read_settings(path=settings_path)
         self.client = OpenAI(api_key = self.settings["API_Key"])
         self.pdf = None
         self.summary = None
+        self.all_summaries = []
         self.audio = None
+        self.pdf_maker = portfolio_maker
 
     def read_settings(self, path='settings.csv'):
         """
@@ -51,6 +70,7 @@ class ResearchAssistant:
             print(f"Error reading PDF {path}: {e}")
             self.pdf = None
 
+    
     def create_summary(self):
         """
         Creates a summary of the provided text.
@@ -59,20 +79,22 @@ class ResearchAssistant:
             print("No PDF file available.")
             return
         
-        lang = self.settings["Summary_Language"]
+        lang = self.settings['Inference_Language']
         suffix = f" Please answer in {lang}." if lang != "English" else ""
         
         instruction = self.settings["LLM_Instruction"]
         prompt = f"{self.settings['LLM_Prompt']}{suffix} \n\n {self.pdf}"
         
         try:
-            self.summary = self.client.chat.completions.create(
-                model = self.settings["GPT_Model"],
+            summary = self.client.chat.completions.create(
+                model = self.settings["GPT_Summarizer_Model"],
                 messages=[
                     {"role": "system", "content": instruction},
                     {"role": "user", "content": prompt}
                 ]
-            )
+            ).choices[0].message.content
+            self.summary = summary
+            self.all_summaries.append(summary)
         except Exception as e:
             print(f"Error creating summary: {e}")
 
@@ -83,7 +105,7 @@ class ResearchAssistant:
         if self.summary:
             try:
                 with open(filename, 'w') as file:
-                    file.write(self.summary.choices[0].message.content)
+                    file.write(self.summary)
             except Exception as e:
                 print(f"Error saving summary: {e}")
 
@@ -97,42 +119,67 @@ class ResearchAssistant:
             return
 
         try:
+            voice = random.choice(['alloy', 'echo', 'fable', 'onyx', 'nova', 'shimmer']) if self.settings['TTS_Voice'] == 'shuffle' else self.settings['TTS_Voice']
             self.audio = self.client.audio.speech.create(
-                model = self.settings["TTS_Model"],
-                voice = self.settings["TTS_Voice"],
-                speed = float(self.settings["TTS_Speed"]),
-                input = self.summary.choices[0].message.content,
+                model = self.settings['TTS_Model'],
+                voice = voice,
+                speed = float(self.settings['TTS_Speed']),
+                input = self.summary,
             )
-            self.audio.stream_to_file(filename + self.settings["Audio_Format"])
+            self.audio.stream_to_file(filename + self.settings['Audio_Format'])
         except Exception as e:
             print(f"Error creating audio: {e}")
 
-    def send_email(self):
+    def relace_mail_body_with_newsletter_text(self):
+        """
+        Creates a newsletter text based on the summaries.
+        """
+        lang = self.settings["Summary_Language"]
+        suffix = f" Please answer in {lang}." if lang != "English" else ""
+        
+        instruction = self.settings['Newsletter_Prompt']
+        prompt = f"{' \n\nNext text:\n\n'.join(self.all_summaries)}{suffix}"
+        
+        try:
+            newsletter_text = self.client.chat.completions.create(
+                model = self.settings['GTP_Newsletter_Model'],
+                messages=[
+                    {"role": "system", "content": instruction},
+                    {"role": "user", "content": prompt}
+                ]).choices[0].message.content
+            self.settings['Email_Body'] = newsletter_text
+        except Exception as e:
+            print(f"Error creating newsletter: {e}")
+
+    def send_email(self, build_portfolio=False):
         """
         Sends an email with the specified settings and attachments.
         """
         try:
             msg = MIMEMultipart()
-            msg['From'] = self.settings["Email_From"]
-            msg['To'] = self.settings["Email_To"]
-            msg['Subject'] = self.settings["Email_Subject"]
-            msg.attach(MIMEText(self.settings["Email_Body"], 'plain', 'utf-8'))
+            msg['From'] = self.settings['Email_From']
+            msg['To'] = self.settings['Email_To']
+            msg['Subject'] = self.settings['Email_Subject']
+            msg.attach(MIMEText(self.settings['Email_Body'], 'plain', 'utf-8'))
 
             # Attach files
-            for file_path in glob.glob(os.path.join(self.settings["Destination_Directory"], f'*{self.settings["Audio_Format"]}')):
+            relevant_files = glob.glob(os.path.join(self.settings['Destination_Directory'], f'*{self.settings['Audio_Format']}'))
+            if build_portfolio:
+                relevant_files.append(*glob.glob(os.path.join(self.settings['Destination_Directory'], '*.pdf')))
+            for file_path in relevant_files:
                 filename = os.path.basename(file_path)
                 with open(file_path, 'rb') as attachment:
                     part = MIMEBase('application', 'octet-stream')
                     part.set_payload(attachment.read())
                 encoders.encode_base64(part)
-                part.add_header('Content-Disposition', f"attachment; filename= {filename}")
+                part.add_header('Content-Disposition', f'attachment; filename= {filename}')
                 msg.attach(part)
 
             # Setup and send the email
-            server = smtplib.SMTP(self.settings["SMTP_Host"], int(self.settings["SMTP_Port"]))
+            server = smtplib.SMTP(self.settings['SMTP_Host'], int(self.settings['SMTP_Port']))
             server.starttls()
             try:
-                server.login(self.settings["SMTP_User"], self.settings["SMTP_Password"])
+                server.login(self.settings['SMTP_User'], self.settings['SMTP_Password'])
                 server.send_message(msg)
             finally:
                 server.quit()
@@ -143,18 +190,26 @@ class ResearchAssistant:
 
         filedir = self.settings["File_Directory"]
         destdir = self.settings["Destination_Directory"]
-        sendmail = self.settings["Send_Email"].lower() == 'true'
+        create_audio = self.settings["create_audio"].lower() == 'true'
+        create_newsletter = self.settings["create_newsletter"].lower() == 'true'
+        sendmail = self.settings["send_email"].lower() == 'true'
         unlink = self.settings["remove_pdfs_after_process"].lower() == 'true'
+        build_portfolio = self.settings["build_portfolio"].lower() == 'true'
         all_files = glob.glob(os.path.join(filedir, '*.pdf'))
-
+        
         # double check: create destdir if it does not exist
         if not os.path.exists(destdir):
             os.makedirs(destdir)
+
+        if build_portfolio:
+            pdf = self.pdf_maker
+            pdf.add_page()
 
         for file_path in all_files:
             
             # get file name
             file = os.path.basename(file_path)
+            root_name = os.path.splitext(file)[0]
             
             # read PDF file
             print('read PDF file:', file)
@@ -165,26 +220,40 @@ class ResearchAssistant:
             self.create_summary()
 
             # save summary locally
-            filename = os.path.join(destdir, os.path.splitext(file)[0] + ".txt")
+            filename = os.path.join(destdir, root_name + ".txt")
             self.save_summary(filename = filename)
 
             # create audio from summary
-            print('create audio file')
-            filename = os.path.join(destdir, os.path.splitext(file)[0])
-            self.create_audio_from_summary(filename = filename)
+            if create_audio:
+                print('create audio file')
+                filename = os.path.join(destdir, root_name)
+                self.create_audio_from_summary(filename = filename)
 
             # remove file from folder
             if unlink:
                 print('remove file')
                 os.remove(file_path)
 
+        # create newsletter
+        if create_newsletter:
+            print('create newsletter')
+            self.relace_mail_body_with_newsletter_text()
+
+        # build and save portfolio locally
+        if build_portfolio:
+            print('build pdf portfolio')
+            filenames = [os.path.splitext(os.path.basename(file))[0] for file in all_files]
+            for summary, meta in zip(self.all_summaries, filenames):
+                title = meta.encode('latin-1', 'replace').decode('latin-1')
+                body = summary.encode('latin-1', 'replace').decode('latin-1')
+                pdf.chapter_title(title)
+                pdf.chapter_body(body)
+            pdf.output(os.path.join(destdir, 'Portfolio.pdf'))
+
         # send email
-        if sendmail: self.send_email()
+        if sendmail: self.send_email(build_portfolio=build_portfolio)
 
 
-
-
-## Run Code
-            
-assi = ResearchAssistant(settings_path='settings.csv')
+# run code
+assi = ResearchAssistant('settings.csv', portfolio_maker=PDF())
 assi.read_and_summarize_pdf()
